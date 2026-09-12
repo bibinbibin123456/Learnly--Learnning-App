054d222f4e00ecb4143dea1d5122f84a068ce35d
bibinbibin123456 <bibinpbabu12@gmailcom>
2026-09-12 19:20:29 +0530

    Update CORS configuration

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cookieparser())
 
 app.use(cors({
-    origin:  'https://learnly-learnningapp-frontend.vercel.app/courses',
+    origin:  'https://learnly-learnningapp-frontend.vercel.app',
     credentials:true
 }))
 
